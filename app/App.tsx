95277c739dbe32376b78c50b784ce76e58b05a23
ratmastererik <erik.lindgrenw@gmail.com>
2025-09-23 11:32:23 +0200

    feat: add location tracking and map component

--- app/App.tsx
+++ app/App.tsx
@@ -1,10 +1,36 @@
+import * as Location from "expo-location";
 import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import Map from "../components/maps";
 
 export default function App() {
+  const [location, setLocation] = useState<Location.LocationObject>();
+  const [errorMsg, setErrorMsg] = useState<string>("");
+
+  useEffect(() => {
+    async function getCurrentLocation() {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      Location.watchPositionAsync({}, (loc) => {
+        console.log(loc.coords.latitude);
+        console.log(loc.coords.longitude);
+        console.log(loc?.timestamp);
+        setLocation(loc);
+      });
+    }
+    getCurrentLocation();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{location?.timestamp}</Text>
+      <Map />
       <StatusBar style="auto" />
     </View>
   );
